8bedde01d8badaf37f730f12cc53674e5a0b7736
Alexander Nyberg <alexandernyberg@me.com>
2023-10-03 17:46:44 +0200

    Initial commit

---
+++ chocolatey.sh
@@ -0,0 +1,17 @@
+### Install file for package handler Chocolatey for Windows environments
+###
+###
+
+# Install Chocolatey
+iwr -useb community.chocolatey.org/install.ps1 | iex
+
+# Allow Global Confirmation
+choco feature enable -n allowGlobalConfirmation
+
+# Install Bulk Crap Uninstaller
+# Used for uninstalling multiple programs at once.
+choco install bulk-crap-uninstaller
+
+# Install pyenv for Windows
+choco install pyenv-win
+pyenv update
